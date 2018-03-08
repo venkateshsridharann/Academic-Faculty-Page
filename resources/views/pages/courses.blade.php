37eef629f0d29d4c00beb906e4fe594653f45dd6
venkatesh <vs498@njit.edu>
2018-03-08 05:59:52 -0500

    center align courses page

--- resources/views/pages/courses.blade.php
+++ resources/views/pages/courses.blade.php
@@ -6,8 +6,8 @@
 <div class="container text-left padding-top">
     <h1 class="text-center"> Courses</h1>
     <hr>
-
-    <ul class="courses" style="margin-left: 10em; padding-top: 20px;">
+    <br>
+    <ul class="courses font" style=" padding-top: 20px;">
         <li>FED 101: Fundamental of Engineering Design</li>
         <li>EE 413: Introduction to Professional Engineering</li>
         <li>ECE 650: Analog Integrated Circuit Design</li>
